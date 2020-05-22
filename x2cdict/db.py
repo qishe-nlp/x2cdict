d1308b7bb60cdf42e1b61acfc379a44110977a10
Phoenix.Grey <phoenix.grey0108@gmail.com>
2020-05-22 20:40:51 +0800

    update verb extensions

--- x2cdict/db.py
+++ x2cdict/db.py
@@ -7,7 +7,18 @@ class DB:
     self.client = MongoClient(uri)
     self.db = self.client[dbname]
     self.vocabs = self.db.vocabs
+    self.verbs_variation = self.db.verbs_variation
+    self.verbs_inverse_variation = self.db.verbs_inverse_variation
 
   def search_vocab(self, text):
     result = self.vocabs.find_one({"word": text})
     return result 
+
+  def search_verbs_variation(self, text):
+    result = self.verbs_variation.find_one({"word": text})
+    return result 
+
+  def search_verbs_inverse_variation(self, text):
+    result = self.verbs_inverse_variation.find_one({"word": text})
+    return result 
+
--- x2cdict/dictapi.py
+++ x2cdict/dictapi.py
@@ -14,7 +14,7 @@ class VocabDict:
     self.api = None
     if self.dictname in self.__class__.APIS:
       # TODO: move auth into configuration
-      self.api = DB(self.dictname, "phoenix", "turingmachine", "192.168.21.2")
+      self.api = DB(self.dictname, "phoenix", "turingmachine", "192.168.21.50")
       self.posmap = POSMAP[from_lang]
 
   def word(self, w, pos=None):
@@ -39,23 +39,70 @@ class VocabDict:
     }
     return result
 
-  def api_search(self, w, pos):
-    _pos = self.posmap[pos] if pos in self.posmap.keys() else [pos] 
-    _r = self.api.search_vocab(w)
-
+  def api_search_verb_original(self, w, _pos):
     result = None
-    if _r != None:
-      ex = _r["explaination"]
+    verb = self.api.search_vocab(w)
+    if verb != None:
+      ex = verb["explaination"]
 
-      e = ex[0]
+      e = None
       for i in ex:
         if i["pos"] in _pos:
           e = i
           break
  
+      if e != None:
+        result = {
+          "word": w,
+          "explaination": e,
+          "from": self.dictname 
+        }
+
+        vv = self.api.search_verbs_variation(w) 
+        if vv != None:
+          result["extension"] = vv["extension"] 
+
+      return result
+
+  def api_search_verb_variation(self, w, _pos):
+    verb = self.api.search_verbs_inverse_variation(w)
+    result = None
+    if verb != None:
+      origin_verb = verb["extension"]["origin"]
+      info = self.api_search_verb_original(origin_verb, _pos)
       result = {
         "word": w,
-        "explaination": e,
-        "from": self.dictname 
+        "explaination": info["explaination"],
+        "variations": verb["extension"]["variations"],
       }
+      if "extension" in info.keys():
+        result["extension"] = info["extension"]
+    return result
+
+  def api_search(self, w, pos):
+    _pos = self.posmap[pos] if pos in self.posmap.keys() else [pos] 
+
+    result = None
+    if pos == "VERB":
+      result = self.api_search_verb_original(w, _pos)
+      if result == None:
+        result = self.api_search_verb_variation(w, _pos)
+    else:
+      _r = self.api.search_vocab(w)
+
+      if _r != None:
+        ex = _r["explaination"]
+
+        e = ex[0]
+        for i in ex:
+          if i["pos"] in _pos:
+            e = i
+            break
+   
+        result = {
+          "word": w,
+          "explaination": e,
+          "from": self.dictname 
+        }
+
     return result
